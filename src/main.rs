bd61ec8dc17d696a4d4598bcea173b04f3da1b74
Thomas <tomullender@gmail.com>
2022-12-01 23:07:36 +0000

    Initial

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(unused_attributes)]
+#![allow(non_camel_case_types)]
+#![allow(unused_parens)]
+
+fn main() {
+    println!("Hello, world!");
+}
